c2d1e5b1dbbd638fa575e0e9d9a2a1cf7b1afb24
OberoiDhruv <naatitest.ai@gmail.com>
2024-12-01 20:08:06 +1100

    logo size

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -10,7 +10,7 @@ const Logo = ({ isMobile }: LogoProps) => {
     <Link href={'/'}>
       <div className="flex  items-center">
         <div className="flex justify-center items-center">
-          <Image src="/logo.png" alt="Naati Ninga" width={173} height={173} />
+          <Image src="/logo.png" alt="Naati Ninga" width={101} height={101} />
         </div>
         {!isMobile ? (
           <h1 className="shadows  text-primary text-[32px] sm:text-[35px]"></h1>
