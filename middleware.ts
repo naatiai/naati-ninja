3f9dbf761487708cde29f9550387c9519c939a41
OberoiDhruv <naatitest.ai@gmail.com>
2025-03-06 11:22:07 +1100

    clerk auth removed for webhook

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,16 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/api/pay/webhook', '/sign-in', '/sign-up', '/api/clerk/webhook'],
+  publicRoutes: [
+    '/',
+    '/api/pay/webhook',
+    '/sign-in',
+    '/sign-up',
+    '/api/clerk/webhook',
+    '/api/clerk/user',
+  ],
 });
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-}
+};
